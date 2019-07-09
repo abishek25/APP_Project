6c9d8fe3bebbeb64526c5715f06d9c13d3cc898e
Dinesh <dineshpattapu@gmail.com>
2019-07-09 14:52:30 -0700

    qwe

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -7,11 +7,19 @@ public class GameController {
 	
 	Game game;
 	
-	public void createGame(String player1Name) {
-		game = new Game(player1Name);
+	public void createGame(String player1Name, String positions1) {
+		game = new Game(player1Name, positions1);
 	}
 	
 	public Game getGame() {
 		return game;
+	}
+	
+	public Player getCurrPlayer() {
+		return game.getCurrPlayer();
+	}
+	
+	public String processAttack(int row, int col) {
+		return game.processAttack(row, col);
 	}
 }
--- src/model/Game.java
+++ src/model/Game.java
@@ -1,27 +1,39 @@
 package model;
 
+import java.util.Random;
+
 public class Game {
 	
 	public static int GAME_MODE_AI = 1;
 	public static int GAME_MODE_NETWORK = 2;
 	public static int GAME_MODE_INVALID = 3;
+	public static Random rand = new Random();
 	
 	Player[] players;
 	int mode;
 	int currPlayer;
 	
-	public Game(String playerOneName) {
+	public static boolean checkPositions(String positions) {
+		return true;
+	}
+	
+	public String randomShipPositions() {
+		String aiPositions = "(B1,B5),(D2,D5),(C3,C5),(F4,F6),(G2,G3)";
+		return aiPositions;
+	}
+	
+	public Game(String playerOneName, String player1Positions) {
 		players = new Player[2];
-		players[0] = new Player(playerOneName);
-		players[1] = new Player("AI");
+		players[0] = new Player(playerOneName, player1Positions);
+		players[1] = new Player("AI", randomShipPositions());
 		mode = GAME_MODE_AI;
 		currPlayer = 0;
 	}
 	
-	public Game(String playerOneName, String playerTwoName) {
+	public Game(String playerOneName, String player1Positions, String playerTwoName, String player2Positions) {
 		players = new Player[2];
-		players[0] = new Player(playerOneName);
-		players[1] = new Player(playerTwoName);
+		players[0] = new Player(playerOneName, player1Positions);
+		players[1] = new Player(playerTwoName, player2Positions);
 		mode = GAME_MODE_NETWORK;
 		currPlayer = 0;
 	}
@@ -39,5 +51,13 @@ public class Game {
 			return players[1].getBoard();
 		}
 		return null;
+	}
+	
+	public Player getCurrPlayer() {
+		return this.players[currPlayer];
+	}
+	
+	public String processAttack(int row, int col) {
+		return "";
 	}
 }
