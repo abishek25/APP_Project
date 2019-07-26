0c128e84805f20cd891ebdebfe2d4eff502d2e06
Dinesh <dineshpattapu@gmail.com>
2019-07-25 21:56:01 -0400

    Drag and Drop of Ship

--- src/view/Window.java
+++ src/view/Window.java
@@ -2,14 +2,20 @@ package view;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.TransferHandler;
 
 import controller.GameController;
 import model.Board;
@@ -30,6 +36,13 @@ public class Window {
 	JLabel resultLabel;
 	JLabel gameStatus;
 	
+	JLabel placeShip1;
+	JLabel placeShip2;
+	JLabel placeShip3;
+	JLabel placeShip4;
+	JLabel placeShip5;
+	JLabel toMoveShip;
+	
 	/**
 	 * Function to create the file's new game menu item
 	 * @return The menu item
@@ -42,6 +55,8 @@ public class Window {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				String player1 = JOptionPane.showInputDialog("Please input name for player 1: ");
+				
+				/*
 				String positions1 = JOptionPane.showInputDialog(""
 						+ "Please input placement positions for\n"
 						+ "Carrier(5),Battleship(4),Cruiser(3),Submarine(3),Destroyer(2) respectively\n"
@@ -51,6 +66,9 @@ public class Window {
 				while(Game.checkPositions(positions1) == false) {
 					positions1 = JOptionPane.showInputDialog("Incorrect positions. Try again: ");
 				}
+				*/
+				
+				String positions1 = "";
 				
 				gameController = new GameController();
 				gameController.createGame(player1, positions1);
@@ -192,6 +210,30 @@ public class Window {
 			}
 		}
 		
+		placeShip1 = new JLabel("SHIP#SHIP#SHIP#SHIP#SHIP");
+		placeShip2 = new JLabel("SHIP#SHIP#SHIP#SHIP");
+		placeShip3 = new JLabel("SHIP#SHIP#SHIP");
+		placeShip4 = new JLabel("SHIP#SHIP#SHIP");
+		placeShip5 = new JLabel("SHIP#SHIP");
+		
+		placeShip1.addMouseListener(lblDnd);
+		placeShip2.addMouseListener(lblDnd);
+		placeShip3.addMouseListener(lblDnd);
+		placeShip4.addMouseListener(lblDnd);
+		placeShip5.addMouseListener(lblDnd);
+		
+		placeShip1.setBounds(400, 670, 200, 10);
+		placeShip2.setBounds(400, 690, 200, 10);
+		placeShip3.setBounds(400, 710, 200, 10);
+		placeShip4.setBounds(400, 730, 200, 10);
+		placeShip5.setBounds(400, 750, 200, 10);
+		
+		f.add(placeShip1);
+		f.add(placeShip2);
+		f.add(placeShip3);
+		f.add(placeShip4);
+		f.add(placeShip5);
+		
 		refreshBoard();
 	}
 
@@ -224,4 +266,144 @@ public class Window {
 		win.start();
 	}
 
+	MouseListener lblDnd = new MouseListener() {
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			//toMoveShip.setBounds(e.getXOnScreen() - 5, e.getYOnScreen() - 60, 200, 10);
+			
+			int minx = 0;
+			int miny = 0;
+			
+			int targetx = 0;
+			int targety = 0;
+			
+			for(int i = 0; i < Board.BOARD_ROWS; i++) {
+				for(int j = 0; j < Board.BOARD_COLS; j++) {
+					JLabel tmpLbl = lblShipPlacementGrid[i][j];
+					int diffx = Math.abs(tmpLbl.getX() - e.getXOnScreen());
+					int diffy = Math.abs(tmpLbl.getY() - e.getYOnScreen() + 60);
+					
+					if(minx == 0 && miny == 0) {
+						minx = diffx;
+						miny = diffy;
+						targetx = tmpLbl.getX();
+						targety = tmpLbl.getY();
+					}
+					else if(minx > diffx || miny > diffy) {
+						minx = diffx;
+						miny = diffy;
+						targetx = tmpLbl.getX();
+						targety = tmpLbl.getY();
+					}
+				}
+			}
+			
+			for(int i = 0; i < Board.BOARD_ROWS; i++) {
+				for(int j = 0; j < Board.BOARD_COLS; j++) {
+					
+					JLabel tmpLbl = lblShipPlacementGrid[i][j];
+					
+					if(tmpLbl.getX() == targetx && tmpLbl.getY() == targety) {
+						String[] numShips = toMoveShip.getText().split("#");
+						if(numShips.length == 5) {
+							if(i > (Board.BOARD_ROWS - 5)) {
+								JOptionPane.showMessageDialog(null, "Not enough space to put the selected ship. Please try again");
+							}
+							else {
+								boolean place = true;
+								for(int k = i; k < (i + 5); k++) {
+									if(lblShipPlacementGrid[k][j].getText().equals("SHIP")) {
+										JOptionPane.showMessageDialog(null, "Area occupied by another ship. Please try again");
+										place = false;
+										break;
+									}
+								}
+								if(place == true) {
+									for(int k = i; k < (i + 5); k++) {
+										lblShipPlacementGrid[k][j].setText("SHIP");
+									}
+								}
+							}
+						}
+						else if(numShips.length == 4) {
+							if(i > (Board.BOARD_ROWS - 4)) {
+								JOptionPane.showMessageDialog(null, "Not enough space to put the selected ship. Please try again");
+							}
+							else {
+								boolean place = true;
+								for(int k = i; k < (i + 4); k++) {
+									if(lblShipPlacementGrid[k][j].getText().equals("SHIP")) {
+										JOptionPane.showMessageDialog(null, "Area occupied by another ship. Please try again");
+										place = false;
+										break;
+									}
+								}
+								if(place == true) {
+									for(int k = i; k < (i + 4); k++) {
+										lblShipPlacementGrid[k][j].setText("SHIP");
+									}
+								}
+							}
+						}
+						else if(numShips.length == 3) {
+							if(i > (Board.BOARD_ROWS - 3)) {
+								JOptionPane.showMessageDialog(null, "Not enough space to put the selected ship. Please try again");
+							}
+							else {
+								boolean place = true;
+								for(int k = i; k < (i + 3); k++) {
+									if(lblShipPlacementGrid[k][j].getText().equals("SHIP")) {
+										JOptionPane.showMessageDialog(null, "Area occupied by another ship. Please try again");
+										place = false;
+										break;
+									}
+								}
+								if(place == true) {
+									for(int k = i; k < (i + 3); k++) {
+										lblShipPlacementGrid[k][j].setText("SHIP");
+									}
+								}
+							}
+						}
+						else if(numShips.length == 2) {
+							if(i > (Board.BOARD_ROWS - 2)) {
+								JOptionPane.showMessageDialog(null, "Not enough space to put the selected ship. Please try again");
+							}
+							else {
+								boolean place = true;
+								for(int k = i; k < (i + 2); k++) {
+									if(lblShipPlacementGrid[k][j].getText().equals("SHIP")) {
+										JOptionPane.showMessageDialog(null, "Area occupied by another ship. Please try again");
+										place = false;
+										break;
+									}
+								}
+								if(place == true) {
+									for(int k = i; k < (i + 2); k++) {
+										lblShipPlacementGrid[k][j].setText("SHIP");
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			
+			toMoveShip.setBounds(targetx, targety, 200, 10);
+			toMoveShip = null;
+		}
+		
+		@Override
+		public void mousePressed(MouseEvent e) {
+			toMoveShip = (JLabel)e.getSource();
+		}
+		
+		@Override public void mouseExited(MouseEvent e) {}
+		
+		@Override public void mouseEntered(MouseEvent e) {}
+		
+		@Override public void mouseClicked(MouseEvent e) {}
+	};
+	
+	
 }
