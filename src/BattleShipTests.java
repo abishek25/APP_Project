6ea2d4e1dfb70f1ec622e062b1b6adc441b17443
Abishek <saiabishekthorikonda20@gmail.com>
2019-07-31 10:49:23 -0400

    Unit Test

--- src/BattleShipTests.java
+++ src/BattleShipTests.java
@@ -1,5 +1,9 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import controller.GameController;
@@ -14,174 +18,321 @@ import view.Window;
  *
  */
 class BattleShipTests {
+	
+	Window win;
 
+	@BeforeEach 
+	public void setup() {
+		win = new Window();
+		win.lblAttackGrid = new JButton[Board.BOARD_ROWS][Board.BOARD_COLS];
+		win.lblShipPlacementGrid = new JLabel[Board.BOARD_ROWS][Board.BOARD_COLS];
+		
+		for(int i = 0; i < Board.BOARD_ROWS; i++) {
+			for(int j = 0; j < Board.BOARD_COLS; j++) {
+				win.lblAttackGrid[i][j] = new JButton("");
+				win.lblShipPlacementGrid[i][j] = new JLabel("-----");
+			}
+		}
+		
+		win.gameController = new GameController(1);
+		win.board = new Board();
+		win.playerShips = new String[5];
+		Window.DEV_TEST = 5;
+	}
+	
 	/**
-	 * Test one to check initHoles
+	 * Test one to vertical ship placement
 	 */
 	@Test
-	void test1() {
-		Ship ship = new Ship("test", 4);
-		ship.initHoles(4);
-		for(int i = 0; i < 4; i++) {
-			assertFalse(ship.holes[i]);
+	void testShipPlacementVertical() {
+		String shipText = "SHIP#SHIP#SHIP#SHIP#SHIP";
+		win.processShipPlacementVertically(0, 0, shipText);
+		
+		int i = 0;
+		for(int j = 0; j<0; j++) {
+			assertEquals("SHIP", win.lblShipPlacementGrid[i][j]);
 		}
 	}
 	
+	/**
+	 * Test one to check horizontal ship placement
+	 */
 	@Test
-	void test2() {
-		Ship ship = new Ship("test", 4);
-		ship.initHoles(4);
-		assertFalse(ship.isShipSunk());
+	void testShipPlacementHorizontal() {
+		String shipText = "SHIP#SHIP#SHIP#SHIP#SHIP";
+		win.processShipPlacementHorizontally(0, 0, shipText);
+		
+		int i = 0;
+		for(int j = 0; j<0; j++) {
+			assertEquals("SHIP", win.lblShipPlacementGrid[j][i]);
+		}
 	}
 	
+	/**
+	 * Test the placement of ship one on other
+	 */
 	@Test
-	void test3() {
-		Ship ship = new Ship("test", 4);
-		ship.initHoles(4);
-		assertTrue(ship.shipHit());
+	void testShipPlacementConstraintOne() {
+		String shipText = "SHIP#SHIP#SHIP#SHIP#SHIP";
+		win.processShipPlacementHorizontally(0, 0, shipText);
+		Boolean result = win.processShipPlacementHorizontally(0, 0, shipText);
+		assertEquals(result, false);
 	}
 	
+	/**
+	 * Test the placement of ship in same row
+	 */
 	@Test
-	void test4() {
-		Ship ship = new Ship("test", 4);
-		ship.initHoles(4);
-		ship.shipHit();
-		assertTrue(ship.holes[0]);
+	void testShipPlacementConstraintTwo() {
+		String shipText = "SHIP#SHIP#SHIP#SHIP#SHIP";
+		win.processShipPlacementHorizontally(0, 0, shipText);
+		shipText = "SHIP#SHIP#SHIP#SHIP";
+		Boolean result = win.processShipPlacementHorizontally(5, 0, shipText);
+		assertEquals(result, false);
 	}
 	
+	/**
+	 * Test the placement of ship outside board
+	 */
 	@Test
-	void test5() {
-		Player p = new Player("playerone", "");
-		assertEquals(p.ships.length, 5);
+	void testShipPlacementConstraintThree() {
+		String shipText = "SHIP#SHIP#SHIP#SHIP#SHIP";
+		Boolean result = win.processShipPlacementHorizontally(9, 0, shipText);
+		assertEquals(result, false);
 	}
 	
+	/**
+	 * Test the AI Ship Placement
+	 */
 	@Test
-	void test6() {
-		Player p = new Player("playerone", "");
-		assertEquals(p.getName(), "playerone");
+	void testAIShipPlacement() {
+		win.gameController.createGame("Tesst", win.board, win.playerShips);
+		String positions = win.gameController.getGame().randomShipPositions();
+		assertTrue(positions.split(",").length == 10);
 	}
 	
+	/**
+	 * Test the AI Attack
+	 */
 	@Test
-	void test7() {
-		Player p = new Player("playerone", "");
-		assertNotNull(p.getBoard());
+	void testAIAttack() {
+		win.gameController.createGame("Tesst", win.board, win.playerShips);
+		String result = win.gameController.getGame().generateAiAttack();
+		assertTrue(result.split("#").length == 3);
 	}
 	
+	/**
+	 * Test startup player turn
+	 */
 	@Test
-	void test8() {
-		Game g = new Game("p1", "", "p2", "");
-		assertEquals(g.getGameMode(), 2);
+	void testStartUpPlayerTurn() {
+		win.gameController.createGame("Tesst", win.board, win.playerShips);
+		String playerName = win.gameController.getGame().getCurrPlayer().getName();
+		assertEquals(playerName, "Tesst");
 	}
 	
+	/**
+	 * Test startup player scores
+	 */
 	@Test
-	void test9() {
-		Game g = new Game("p1", "", "p2", "");
-		assertNotNull(g.getGameBoard());
+	void testStartUpPlayerScores() {
+		win.gameController.createGame("Tesst", win.board, win.playerShips);
+		assertEquals(win.gameController.getGame().getPlayerOneResults(), 0);
+		assertEquals(win.gameController.getGame().getPlayerTwoResults(), 0);
 	}
 	
+	/**
+	 * Test startup game mode
+	 */
 	@Test
-	void test10() {
-		Game g = new Game("p1", "", "p2", "");
-		assertEquals(g.getCurrPlayer().getName(), "p1");
+	void testStartUpGameMode() {
+		win.gameController.createGame("Tesst", win.board, win.playerShips);
+		assertEquals(win.gameController.getGame().getGameMode(), Game.GAME_MODE_AI);
 	}
 	
+	/**
+	 * Test startup players
+	 */
 	@Test
-	void test11() {
-		Game g = new Game("p1", "", "p2", "");
-		assertEquals(Game.checkIfGameWon(), false);
+	void testStartUpPlayers() {
+		win.gameController.createGame("Tesst", win.board, win.playerShips);
+		assertEquals(win.gameController.getGame().players.length, 2);
 	}
 	
+	/**
+	 * Test startup Ship Placement Grid Creation
+	 */
 	@Test
-	void test12() {
-		Game g = new Game("p1", "", "p2", "");
-		assertEquals(g.getPlayerTwoResults(), 0);
-	}
-	
-	@Test
-	void test13() {
-		Game g = new Game("p1", "", "p2", "");
-		assertEquals(g.getPlayerOneResults(), 0);
-	}
-	
-	@Test
-	void test14() {
+	void testStartUpBoardCreationOne() {
 		Board b = new Board();
-		assertEquals(b.ship_placement_grid[0][0], Board.BOARD_EMPTY);
+		for(int i = 0; i < Board.BOARD_ROWS; i++) {
+			for(int j = 0; j < Board.BOARD_COLS; j++) {
+				assertEquals(b.ship_placement_grid[i][j], Board.BOARD_EMPTY);
+			}
+		}
 	}
 	
+	/**
+	 * Test Startup Attack Grid Creation
+	 */
 	@Test
-	void test15() {
+	void testStartUpBoardCreationTwo() {
 		Board b = new Board();
-		assertEquals(b.getAttackGrid()[0][0], Board.BOARD_EMPTY);
+		for(int i = 0; i < Board.BOARD_ROWS; i++) {
+			for(int j = 0; j < Board.BOARD_COLS; j++) {
+				assertEquals(b.getAttackGrid()[i][j], Board.BOARD_EMPTY);
+			}
+		}
 	}
 	
+	/**
+	 * Test Attack on ship
+	 */
 	@Test
-	void test16() {
+	void testShipAttack() {
 		Board b = new Board();
 		b.handleShipAttack(0, 0);
 		assertEquals(b.ship_placement_grid[0][0], Board.PLACEMENT_BOARD_SHIP_HIT);
 	}
 	
+	/**
+	 * Test startup UI Creation File Menu
+	 */
 	@Test
-	void test17() {
-		GameController gc = new GameController(1);
-		assertEquals(gc.gameMode, 1);
-	}
-	
-	@Test
-	void test18() {
-		GameController gc = new GameController(1);
-		assertNull(gc.getGame());
-	}
-	
-	@Test
-	void test19() {
-		String s[] = {"1,1,1,1,1,1","1,1,1,1,1,1","1,1,1,1,1,1","1,1,1,1,1,1","1,1,1,1,1,1"};
-		GameController gc = new GameController(1);
-		gc.createGame("p1", new Board(), s);
-		assertEquals(gc.getP1Score(), 0);
-	}
-	
-	@Test
-	void test20() {
-		String s[] = {"1,1,1,1,1,1","1,1,1,1,1,1","1,1,1,1,1,1","1,1,1,1,1,1","1,1,1,1,1,1"};
-		GameController gc = new GameController(1);
-		gc.createGame("p1", new Board(), s);
-		assertEquals(gc.getAIScore(), 0);
-	}
-	
-	@Test
-	void test21() {
-		String s[] = {"1,1,1,1,1,1","1,1,1,1,1,1","1,1,1,1,1,1","1,1,1,1,1,1","1,1,1,1,1,1"};
-		GameController gc = new GameController(1);
-		gc.createGame("p1", new Board(), s);
-		assertEquals(gc.getCurrPlayer().getName(), "p1");
-	}
-	
-	@Test
-	void test22() {
-		String s[] = {"1,1,1,1,1,1","1,1,1,1,1,1","1,1,1,1,1,1","1,1,1,1,1,1","1,1,1,1,1,1"};
-		GameController gc = new GameController(1);
-		gc.createGame("p1", new Board(), s);
-		assertNotNull(gc.getGame());
-	}
-	
-	@Test
-	void test23() {
+	void testStartupFileMenuCreation() {
 		Window win = new Window();
 		assertNotNull(win.createFileMenu());
 	}
 	
+	/**
+	 * Test startup UI Creation Menu Bar
+	 */
 	@Test
-	void test24() {
+	void testStartUpMenuBarCreation() {
 		Window win = new Window();
 		assertNotNull(win.createMenu());
 	}
 	
+	/**
+	 * Test Startup Player Ship creation
+	 */
 	@Test
-	void test25() {
+	void testPlayerShipCreation() {
 		Window win = new Window();
 		win.initMainFrame();
 		assertEquals(win.playerShips.length, 5);
+	}
+	
+	/**
+	 * Test game winner
+	 */
+	@Test
+	void testGameWinner() {
+		win.gameController.createGame("Tesst", win.board, win.playerShips);
+		for(int i = 0; i < Board.BOARD_ROWS; i++) {
+			for(int j = 0; j < Board.BOARD_COLS; j++) {
+				win.gameController.getGame().processAttack(i, j);
+				if(Game.isFinished == true) {
+					break;
+				}
+			}
+		}
+		assertNotNull(win.gameController.getGame().getWinner());
+	}
+	
+	/**
+	 * Test game attack changing ship placement grid
+	 */
+	@Test
+	void testGameAttackOne() {
+		win.gameController.createGame("Tesst", win.board, win.playerShips);
+		boolean attackSuccess = false;
+		
+		for(int i = 0; i < Board.BOARD_ROWS; i++) {
+			for(int j = 0; j < Board.BOARD_COLS; j++) {
+				win.gameController.getGame().processAttack(i, j);
+				if(win.gameController.getGame().getAiBoard().ship_placement_grid[i][j] == Board.PLACEMENT_BOARD_SHIP_HIT) {
+					attackSuccess = true;
+				}
+			}
+		}
+		
+		assertTrue(attackSuccess);
+	}
+	
+	/**
+	 * Test game attack changing attack grid
+	 */
+	@Test
+	void testGameAttackTwo() {
+		win.gameController.createGame("Tesst", win.board, win.playerShips);
+		win.gameController.getGame().processRegularAttackHit(0,0, win.gameController.getGame().getGameBoard().getAttackGrid());
+		assertEquals(win.gameController.getGame().getGameBoard().getAttackGrid()[0][0], Board.BOARD_HIT);
+	}
+	
+	/**
+	 * Test game attack reset player turn timer
+	 */
+	@Test
+	void testGameAttackThree() {
+		win.gameController.createGame("Tesst", win.board, win.playerShips);
+		win.gameController.getGame().processRegularAttackHit(0,0, win.gameController.getGame().getGameBoard().getAttackGrid());
+		synchronized (win.gameController.getGame().playerTurnTimer) {
+			assertTrue(win.gameController.getGame().playerTurnTimer < 2);
+		}
+	}
+	
+	/**
+	 * Test game attack player score increment
+	 */
+	@Test
+	void testGameAttackFour() {
+		win.gameController.createGame("Tesst", win.board, win.playerShips);
+		win.gameController.getGame().processRegularAttackHit(0,0, win.gameController.getGame().getGameBoard().getAttackGrid());
+		assertEquals(win.gameController.getGame().getPlayerOneResults(), 1);
+	}
+	
+	/**
+	 * Test game attack player score decrement
+	 */
+	@Test
+	void testGameAttackFive() {
+		win.gameController.createGame("Tesst", win.board, win.playerShips);
+		synchronized (win.gameController.getGame().playerTurnTimer) {
+			win.gameController.getGame().playerTurnTimer = 7;
+		}
+		
+		win.gameController.getGame().processRegularAttackMiss(0,0, win.gameController.getGame().getGameBoard().getAttackGrid());
+		assertEquals(win.gameController.getGame().getPlayerOneResults(), -1);
+	}
+	
+	/**
+	 * Test game attack changing attack grid MISS
+	 */
+	@Test
+	void testGameAttackSix() {
+		win.gameController.createGame("Tesst", win.board, win.playerShips);
+		win.gameController.getGame().processRegularAttackMiss(0,0, win.gameController.getGame().getGameBoard().getAttackGrid());
+		assertEquals(win.gameController.getGame().getGameBoard().getAttackGrid()[0][0], Board.BOARD_MISS);
+	}
+	
+	/**
+	 * Test game attack changing ship placement grid for miss
+	 */
+	@Test
+	void testGameAttackSeven() {
+		win.gameController.createGame("Tesst", win.board, win.playerShips);
+		boolean missSuccess = false;
+		
+		for(int i = 0; i < Board.BOARD_ROWS; i++) {
+			for(int j = 0; j < Board.BOARD_COLS; j++) {
+				String result = win.gameController.getGame().processAttack(i, j);
+				if(result.contains("miss")) {
+					missSuccess = true;
+				}
+			}
+		}
+		
+		assertTrue(missSuccess);
 	}
 }
